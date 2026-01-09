639c51a8b5d494303c5d2f41bf90d54f2a858535
Kirtan Bhuta <bhutakirtan7592@gmail.com>
2026-01-09 22:24:39 +0000

    All Pop-up Forms Corner should also, be rounded.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,9 +74,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: `1.5rem`,
+        md: `1rem`,
+        sm: `0.5rem`,
       },
       keyframes: {
         'accordion-down': {
