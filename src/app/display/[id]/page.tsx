f1584c2bf6e96a095f6cf51b22232024436485ba
Kirtan Bhuta <bhutakirtan7592@gmail.com>
2026-01-23 07:09:02 +0000

    In Consultation should not have Groupm Name.

--- src/app/display/[id]/page.tsx
+++ src/app/display/[id]/page.tsx
@@ -178,7 +178,7 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
   }, [queueInfo.nowCalling]);
 
   return (
-    <div className="flex flex-col h-screen w-screen bg-black text-white overflow-hidden">
+    <div className="flex flex-row h-screen w-screen bg-black text-white overflow-hidden">
       <AnimatePresence>
         {queueInfo.nowCalling && (
           <motion.div
@@ -205,8 +205,8 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
         )}
       </AnimatePresence>
       
-      <div className="flex flex-row h-[30vh]">
-        <div className="w-1/2 bg-blue-900 p-2 overflow-hidden">
+      <div className="w-[30vw] h-full flex flex-col">
+        <div className="h-[50vh] bg-blue-900 p-2 overflow-hidden">
           <Card className="h-full bg-transparent border-0 text-white flex flex-col">
             <CardHeader className="p-2">
               <CardTitle className="text-base font-bold text-center text-yellow-300">
@@ -218,7 +218,6 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
                 <TableHeader>
                   <TableRow className="border-b-white/20">
                     <TableHead className="text-white font-semibold py-1 px-2">Cabin</TableHead>
-                    <TableHead className="text-white font-semibold py-1 px-2">Group</TableHead>
                     <TableHead className="text-white font-semibold text-right py-1 px-2">Token</TableHead>
                   </TableRow>
                 </TableHeader>
@@ -226,7 +225,6 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
                   {queueInfo.inConsultation.map((p) => (
                     <TableRow key={p.id} className="border-b-white/10">
                       <TableCell className="py-1 px-2">{p.cabinName}</TableCell>
-                      <TableCell className="py-1 px-2">{p.groupName}</TableCell>
                       <TableCell className="py-1 px-2 font-bold text-lg text-right">{p.tokenNumber}</TableCell>
                     </TableRow>
                   ))}
@@ -235,7 +233,7 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
             </CardContent>
           </Card>
         </div>
-        <div className="w-1/2 bg-gray-800 p-2 overflow-hidden">
+        <div className="h-[50vh] bg-gray-800 p-2 overflow-hidden">
           <Card className="h-full bg-transparent border-0 text-white flex flex-col">
             <CardHeader className="p-2">
               <CardTitle className="text-base font-bold text-center">
@@ -264,7 +262,7 @@ function DisplayPageContent({ params }: { params: { id: string } }) {
         </div>
       </div>
       
-      <div className="h-[70vh] w-full bg-gray-800">
+      <div className="w-[70vw] h-full bg-gray-800">
         <VideoPlayerDisplay advertisements={queueInfo.advertisements} />
       </div>
 
